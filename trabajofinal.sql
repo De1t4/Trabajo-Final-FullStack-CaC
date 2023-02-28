7605ad3d8906f95b3c011691558f5e9088413961
De1t4 <108241180+De1t4@users.noreply.github.com>
2023-02-28 10:49:13 -0300

    Update trabajofinal.sql

--- trabajofinal.sql
+++ trabajofinal.sql
@@ -5,7 +5,7 @@ USE trabajofinal;
 CREATE TABLE users(
 id INT unsigned AUTO_INCREMENT PRIMARY KEY ,
 username VARCHAR(20) NOT NULL,
-password VARCHAR(8) NOT NULL,
+password VARCHAR(20) NOT NULL,
 name VARCHAR(50) NOT NULL,
 last_name VARCHAR(60) NOT NULL,
 email VARCHAR(60) NOT NULL,
